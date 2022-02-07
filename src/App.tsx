d86e974803f08eadef9245fcede1baca362a0f0c
Martin Aleksiev <chochinize@gmail.com>
2022-02-07 21:04:39 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
 
 function App() {
+
+
+interface IState {
+  people:{
+    name:  string;
+    age: number;
+    url: string;
+    note?: string;
+  }[]
+}
+const [people,setPeople] = useState<IState['people']>([])
+people.map(person => {
+  console.log(person.age);
+
+  
+})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my Party</h1>
+      <List people={people}/>
     </div>
   );
 }
